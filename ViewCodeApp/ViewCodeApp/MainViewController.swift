94d253096c5b0edbfd755f35683c0c10ec9add8c
Gilberto Silva <betosilva.luz@gmail.com>
2022-06-19 20:56:50 -0300

    removendo storyboard.

--- ViewCodeApp/ViewCodeApp/MainViewController.swift
+++ ViewCodeApp/ViewCodeApp/MainViewController.swift
@@ -7,7 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
+    
+    override func loadView() {
+        let view = UIView(frame: UIScreen.main.bounds)
+        view.backgroundColor = .red
+        self.view = view
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
